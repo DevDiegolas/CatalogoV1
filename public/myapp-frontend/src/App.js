e12a3f522ca39176e0430a9aa2cba9d37ef417ec
Diego <diegogpssth@gmail.com>
2024-09-13 14:53:44 -0300

    arrumando hamburguer

--- public/myapp-frontend/src/App.js
+++ public/myapp-frontend/src/App.js
@@ -13,7 +13,7 @@ function App() {
 
     const navigateTo = (page) => {
         setCurrentPage(page);
-        setMenuOpen(false);
+        setMenuOpen(false);  // Fecha o menu ao mudar de página
     };
 
     // Função para adicionar item
@@ -85,12 +85,16 @@ function App() {
             <header>
                 <div className="navbar">
                     <h1>Catalogo Online</h1>
-                    <input type="checkbox" id="checkbox" onClick={() => setMenuOpen(!menuOpen)}></input>
-                    <label for="checkbox" className="toggle">
-                        
-                        <div class="bars" id="bar1"></div>
-                        <div class="bars" id="bar2"></div>
-                        <div class="bars" id="bar3"></div>
+                    <input 
+                        type="checkbox" 
+                        id="checkbox" 
+                        checked={menuOpen} 
+                        onChange={() => setMenuOpen(prev => !prev)}
+                    />
+                    <label htmlFor="checkbox" className="toggle">
+                        <div className="bars" id="bar1"></div>
+                        <div className="bars" id="bar2"></div>
+                        <div className="bars" id="bar3"></div>
                     </label>
                     {menuOpen && (
                         <nav className="menu">
@@ -162,9 +166,9 @@ function App() {
                                             <h5 className="nomeitem">{item.nome}</h5>
                                             <p className="descitem">{item.descricao}</p>
                                             <button className="button-delete" onClick={() => handleRemove(item.id)}>
-                                            <svg viewBox="0 0 448 512" className="svgIcon">
-                                                <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path>
-                                            </svg>
+                                                <svg viewBox="0 0 448 512" className="svgIcon">
+                                                    <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path>
+                                                </svg>
                                             </button>
                                         </div>
                                     </div>
